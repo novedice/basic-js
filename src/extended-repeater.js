9ab6e308bf0de053058854dac8554f06d3b93ad5
Natalia <natalia.kitesurf@gmail.com>
2022-10-14 18:15:11 +0100

    feat: transform-array and extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = ''+str;
+  let n = options.repeatTimes,
+      addition = options.addition,
+      sep = options.separator,
+      addRepeat = options.additionRepeatTimes,
+      addSeparator = options.additionSeparator;
+  if (addition === undefined) {
+    addition = '';
+    addRepeat = 0;
+    addSeparator = '';
+  } else {
+    addition = ''+addition;
+    if (addSeparator == undefined) {addSeparator = '|'};
+    if (addRepeat == undefined) {addRepeat = 1};
+  };
+  if (n == undefined) {n=1};
+  if (sep == undefined) {sep = '+'};
+  function newStr (someStr, separ, times) {
+    let arr = [];
+    for (let i = 0; i<times; i++) {
+      arr.push(someStr)
+    };
+    return arr.join(separ);
+  }
+  let s1 = newStr(addition, addSeparator, addRepeat);
+  return newStr((str+s1), sep, n);
 }
 
 module.exports = {
   repeater
 };
+
+console.log(repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }));
+console.log(repeater('la', { repeatTimes: 3 }));
+const objWithSpecificCoercion = {
+  [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+};
+console.log (repeater(objWithSpecificCoercion, { repeatTimes: 2, addition: objWithSpecificCoercion }));
+console.log(repeater(9.234, { repeatTimes: 4, separator: '||', addition: { a: 5 }, additionRepeatTimes: 3, additionSeparator: '&&' }));
+console.log(repeater(-222, { repeatTimes: 4, separator: '||', addition: new Map(), additionRepeatTimes: 3, additionSeparator: '&&' }));
+console.log(repeater(new Set(), { repeatTimes: 3, separator: '??? ', addition: [1, 2, 3, '4'], additionRepeatTimes: 2, additionSeparator: '!!!' }));
+console.log(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }));
+console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }));
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,92 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(Array.isArray(arr))) {throw new Error ('\'arr\' parameter must be an instance of the Array!')};
+  if (!(arr instanceof Array)) {throw new Error ('\'arr\' parameter must be an instance of the Array!')};
+  let doubn = '--double-next',
+      doubp = '--double-prev',
+      disn = '--discard-next',
+      disp = '--discard-prev',
+      arrTransformed = arr.slice(),
+      index;
+  if (!(arr.includes(doubn) || arr.includes(doubp) || arr.includes(disn) || arr.includes(disp))) {return arr};
+  function doubleNext(arrDN) {
+    let arr1=[];
+    index = arrDN.indexOf(doubn);
+    for (let i=0; i<index; i++) {
+      arr1.push(arrDN[i]);
+    };
+    if (index == arrDN.length-1) {return arr1};
+    arr1.push(arrDN[index+1]);
+    arr1.push(arrDN[index+1]);
+    for (let j = index+2; j<arrDN.length; j++) {
+      arr1.push(arrDN[j]);
+    };
+    return arr1;
+ }
+  function doublePrev(arrDP) {
+    index = arrDP.indexOf(doubp);
+    let arr2 = [];
+    if (index == 0) {
+      for (let n = 1; n<arrDP.length; n++) {
+        arr2.push(arrDP[n]);
+      };
+      return arr2;
+    }
+    for (let i=0; i<index-1; i++) {
+      arr2.push(arrDP[i]);
+    };
+    arr2.push(arrDP[index-1]);
+    arr2.push(arrDP[index-1]);
+    for (let j = index+1; j<arrDP.length; j++) {
+      arr2.push(arrDP[j]);
+    };
+    return arr2;
+  };
+  function discardNext (arrDiNext) {
+    index = arrDiNext.indexOf(disn);
+    let arr3 = [];
+    for (let i=0; i<index; i++) {
+      arr3.push(arrDiNext[i]);
+    };
+    if (index == arrDiNext.length-1) {return arr3};
+    arr3.push('delete');
+    for (let j = index+2; j<arrDiNext.length; j++) {
+      arr3.push(arrDiNext[j]);
+    };
+    return arr3;
+  };
+  function discardPrev (arrDiPrev) {
+    index = arrDiPrev.indexOf(disp);
+    let arr4 = [];
+    if (index == 0) {
+      for (let n = 1; n<arrDiPrev.length; n++) {
+        arr4.push(arrDiPrev[n]);
+      };
+      return arr4;
+    }
+    for (let i=0; i<index-1; i++) {
+      arr4.push(arrDiPrev[i]);
+    };
+    arr4.push('delete');
+    for (let j = index+1; j<arrDiPrev.length; j++) {
+      arr4.push(arrDiPrev[j]);
+    };
+    return arr4;
+  };
+  for (let elem of arr) {
+    if (elem == doubn) {arrTransformed = doubleNext(arrTransformed)};
+    if (elem == doubp) {arrTransformed = doublePrev(arrTransformed)};
+    if (elem == disn) {arrTransformed = discardNext(arrTransformed)};
+    if (elem == disp) {arrTransformed = discardPrev(arrTransformed)};
+  };
+  let arrFinal = [];
+  for (let elem of arrTransformed) {
+    if (elem != 'delete') {arrFinal.push(elem)}; 
+  }
+  return arrFinal;
+  
 }
 
 module.exports = {
