28f51cbe6d8631c888999c6e6e62c7746e43e26a
Natalia <natalia.kitesurf@gmail.com>
2022-10-17 23:27:13 +0100

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,10 +18,33 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  if (sampleActivity === undefined) {return false};
-  
+  // console.log('---NEW---', typeof sampleActivity ,'string?', typeof sampleActivity == 'string')
+  let activityValue, result;
+  activityValue = (typeof sampleActivity == 'string') ? parseFloat(sampleActivity): NaN;
+  // console.log('activityValue',activityValue);
+  if (!isNaN(activityValue)) {
+    // ratio ActivityModern/ActivitySample
+    let activityRatio = MODERN_ACTIVITY/activityValue;
+    // console.log('activityRatio', activityRatio);
+    // console.log('Ln', Math.LN2);
+    let koeff = Math.LN2/HALF_LIFE_PERIOD;
+    // console.log('koeff', koeff);
+    result = Math.log(activityRatio)/koeff;
+    // console.log('result before ceil', result);
+    result = Math.ceil(result);
+    if (result <= 0 || !isFinite(result)) {
+      result = false
+    }
+  } else {
+    result = false;
+  }
+  // console.log('result',result);
+  return result;
 }
+
 
 module.exports = {
   dateSample
 };
+// console.log(dateSample('1') ) //22387
+// console.log(dateSample('WOOT!')) // false
